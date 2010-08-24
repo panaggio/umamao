52e725cbf2330a67b5fa9ccd39e934665578ca44
Helder Ribeiro <helder@gmail.com>
2010-08-23 22:12:03 -0300

    Default language for our users is pt

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User
 
   key :notification_opts,         NotificationConfig
 
-  key :language,                  String, :default => "en"
+  key :language,                  String, :default => "pt"
   key :timezone,                  String, :default => "America/Sao_Paulo"
   key :language_filter,           String, :default => "user", :in => LANGUAGE_FILTERS
 
