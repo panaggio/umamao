4962dd5fbcb12ee74a746f241bae0ab7c6214568
Glauber Costa <glauber@umamao.com.br>
2010-08-26 10:38:36 -0300

    make votes signs smaller
    Signed-off-by: Glauber Costa <glauber@umamao.com.br>

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -14,9 +14,9 @@ module VotesHelper
           #{hidden_field_tag "source", source, :id => "source_#{class_name}_#{voteable.id}"}
           <button type="submit" name="vote_up" value="1" class="arrow vote_up">
             #{if vote && vote.value > 0
-                image_tag("vote_up.png", :width => 30, :height => 16, :title => I18n.t("votes.control.have_voted_up"))
+                image_tag("vote_up.png", :width => 15, :height => 10, :title => I18n.t("votes.control.have_voted_up"))
               else
-                image_tag("to_vote_up.png", :width => 30, :height => 16, :title => I18n.t("votes.control.to_vote_up"))
+                image_tag("to_vote_up.png", :width => 15, :height => 10, :title => I18n.t("votes.control.to_vote_up"))
               end
              }
           </button>
@@ -25,9 +25,9 @@ module VotesHelper
           </div>
           <button type="submit" name="vote_down" value="-1" class="arrow vote_down">
             #{if vote && vote.value < 0
-                image_tag("vote_down.png", :width => 30, :height => 16, :title => I18n.t("votes.control.have_voted_down"))
+                image_tag("vote_down.png", :width => 15, :height => 10, :title => I18n.t("votes.control.have_voted_down"))
               else
-                image_tag("to_vote_down.png", :width => 30, :height => 16, :title => I18n.t("votes.control.to_vote_down"))
+                image_tag("to_vote_down.png", :width => 15, :height => 10, :title => I18n.t("votes.control.to_vote_down"))
               end}
           </button>
         </div>
@@ -37,13 +37,13 @@ module VotesHelper
       %@
         <div class='vote_box'>
           <div class="arrow vote_up">
-            #{image_tag("to_vote_up.png", :width => 30, :height => 16)}
+            #{image_tag("to_vote_up.png", :width => 15, :height => 10)}
           </div>
           <div class="votes_average">
             #{calculate_votes_average(voteable)}
           </div>
           <div class="arrow vote_down">
-            #{image_tag("to_vote_down.png", :width => 30, :height => 16)}
+            #{image_tag("to_vote_down.png", :width => 15, :height => 10)}
           </div>
         </div>
       @
