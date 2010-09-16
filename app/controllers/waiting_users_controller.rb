3d5f7938cf01100e81205e6dd7124a91e1760b42
Helder Ribeiro <helder@umamao.com>
2010-09-16 17:33:32 -0300

    Translate mongomapper error messages.

--- app/controllers/waiting_users_controller.rb
+++ app/controllers/waiting_users_controller.rb
@@ -8,6 +8,16 @@ class WaitingUsersController < ApplicationController
     if @waiting_user.save
       redirect_to root_path(:saved => true), :notice => 'Email salvo. Em breve entraremos em contato.'
     else
+      if @waiting_user.errors.present?
+        localized_errors = @waiting_user.errors.map { |field, message|
+          [field, t("validatable.#{message}")]
+        }
+
+        localized_errors.each do |field, message|
+          @waiting_user.errors.replace(field, message)
+        end
+      end
+
       render 'welcome/landing', :layout => false
     end
   end
