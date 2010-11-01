d4607f91d5f8134b4ca550da4eacea0e8720cf6f
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2010-11-01 12:19:24 -0200

    Fixed error in Gravatar plugin.

--- vendor/plugins/gravatar/lib/gravatar.rb
+++ vendor/plugins/gravatar/lib/gravatar.rb
@@ -46,9 +46,9 @@ module GravatarHelper
 
     # Return the HTML img tag for the given email address's gravatar.
     def gravatar(email, options={})
-      src = h(gravatar_url(email, options))
+      src = CGI.escapeHTML(gravatar_url(email, options))
       options = DEFAULT_OPTIONS.merge(options)
-      [:class, :alt, :size].each { |opt| options[opt] = h(options[opt]) }
+      [:class, :alt, :size].each { |opt| options[opt] = CGI.escapeHTML(options[opt].to_s) }
       "<img class=\"#{options[:class]}\" alt=\"#{options[:alt]}\" width=\"#{options[:size]}\" height=\"#{options[:size]}\" src=\"#{src}\" />"
     end
 
@@ -72,7 +72,7 @@ module GravatarHelper
         opts = []
         [:rating, :size, :default].each do |opt|
           unless options[opt].nil?
-            value = h(options[opt])
+            value = CGI.escapeHTML(options[opt].to_s)
             opts << [opt, value].join('=')
           end
         end
