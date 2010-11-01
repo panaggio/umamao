6f0e31806a0375409b51435e27cc9e4d6b8dd807
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2010-11-01 11:46:35 -0200

    Fixed bug in duplicated autocomplete entries.

--- lib/support/autocompletable.rb
+++ lib/support/autocompletable.rb
@@ -12,7 +12,7 @@ module Support::Autocompletable
     def autocompletable_key(key)
       define_method(:update_autocomplete_item) do
         # Builds or updates the corresponding entry in the search index
-        autocomplete_item = AutocompleteItem.find(self.id)
+        autocomplete_item = AutocompleteItem.find_by_entry_id_and_entry_type(self.id, self.class.to_s)
         if autocomplete_item
           if self.send((key.to_s + "_changed?").to_sym)
             autocomplete_item.title = self.send key.to_sym
