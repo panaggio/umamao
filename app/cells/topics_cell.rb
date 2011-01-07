7ab74c871d296b2bddb03e107a18a29e6bf2870d
Arthur Azevedo de Amorim <arthur@umamao.com>
2011-01-07 10:22:19 -0200

    Refactored suggestion widgets.

--- app/cells/topics_cell.rb
+++ app/cells/topics_cell.rb
@@ -2,20 +2,7 @@ class TopicsCell < Cell::Rails
   include Devise::Controllers::Helpers
   helper ApplicationHelper
 
-  def suggestions
-    @user = @opts[:user]
-    @topics = @user.suggested_topics
-    render
-  end
-
-  # Bulk-following of topics (user settings, signup wizard, etc)
-  def follow
-    @user = @opts[:user]
-    @topics = Topic.query(:follower_ids => @user.id).
-      paginate(:per_page => 100, :page => @opts[:page] || 1)
-    render
-  end
-
+  # Used in settings page.
   def followed
     @topic = @opts[:topic]
     render
