e55ccb4ea00529d0aa8ff4baf292403437e716eb
Arthur Azevedo de Amorim <arthur@umamao.com>
2011-01-07 10:24:39 -0200

    Reorganized signup wizard cell.

--- app/cells/signup_wizard_cell.rb
+++ app/cells/signup_wizard_cell.rb
@@ -1,9 +1,38 @@
+# Wizard that is displayed after a user signs up. Steps through
+# external accounts integration, topics and users suggestions.
+
 class SignupWizardCell < Cell::Rails
   include Devise::Controllers::Helpers
   helper ApplicationHelper
 
   def wizard
+    @steps = ["connect", "follow"]
+    @current_step = params[:current_step]
+    current_step_idx = @steps.index(@current_step)
+    @previous_step = current_step_idx == 0 ? nil : @steps[current_step_idx - 1]
+    @next_step = @steps[current_step_idx + 1]
     @user = @opts[:user]
+    render
+  end
+
+  def follow
+    # We begin by populating the suggestions list.
+    if @user.suggested_user_ids.blank?
+      @user.find_users.each do |user|
+        unless @user.following?(user)
+          @user.suggested_users << user
+        end
+      end
+    end
+    if @user.suggested_topic_ids.blank?
+      @user.find_topics.each do |topic|
+        unless topic.follower_ids.include?(@user.id)
+          @user.suggested_topic_ids << topic.id
+        end
+      end
+      @user.suggested_topics_fresh = true
+    end
+    @user.save
     render
   end
 
