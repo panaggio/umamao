5569cbecaf8f23cb56b63045759bd5eac90ffc4e
Helder Ribeiro <helder@umamao.com>
2011-01-10 16:22:52 -0200

    Change default facebook permission to allow posting.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, AppConfig.twitter['key'], AppConfig.twitter['secret'], :sign_in => false
-  provider :facebook, AppConfig.facebook['app_id'], AppConfig.facebook['secret']
+  provider :facebook, AppConfig.facebook['app_id'], AppConfig.facebook['secret'], :scope => 'email,offline_access,publish_stream'
 end
