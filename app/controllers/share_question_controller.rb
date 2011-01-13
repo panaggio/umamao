4bd4bc5034ec444e6c629a6f2c7d75874a3d4dc5
Arthur Azevedo de Amorim <arthur@umamao.com>
2011-01-13 12:12:05 -0200

    Filters in ShareQuestionController.

--- app/controllers/share_question_controller.rb
+++ app/controllers/share_question_controller.rb
@@ -9,7 +9,8 @@ class ShareQuestionController < ApplicationController
         render :json => {
           :success => true,
           :html => (render_cell :share_question, :display,
-                    :question => @question)
+                    :question => @question,
+                    :where => params[:where])
         }
       end
     end
@@ -18,7 +19,10 @@ class ShareQuestionController < ApplicationController
   def create
     @body = params[:body]
     @question = Question.find_by_id(params[:question])
-    if params[:where] == "facebook"
+    @link = question_url(@question)
+
+    case params[:where]
+    when "facebook"
       graph = current_user.facebook_connection
       graph.put_wall_post(@body, :link => question_url(@question))
       status = :success
@@ -27,35 +31,51 @@ class ShareQuestionController < ApplicationController
 
     respond_to do |format|
       format.html do
-        case status
-        when :success
-          redirect_to question_path(@question)
-        when :needs_connection
-          redirect_to settings_external_accounts_path
-        end
+        redirect_to question_path(@question)
       end
 
       format.js do
-        case status
-        when :success
-          render :json => {
-            :success => true,
-            :message => message
-          }.to_json
-        when :needs_connection
-          session["omniauth_return_url"] = "http://www.google.com"
-          render :json => {
-            :success => false,
-            :status => "needs_connection",
-            :html => html,
-            :url => "/auth/facebook"
-          }.to_json
-        end
+        render :json => {
+          :success => true,
+          :message => message
+        }.to_json
       end
     end
   end
 
   def check_connections
+    status = :success
+
+    case params[:where]
+    when "facebook"
+      if !current_user.facebook_account
+        status = :needs_connection
+      end
+    else
+      status = :unknown_destination
+    end
+
+    if status != :success
+      respond_to do |format|
+        format.js do
+          case status
+          when :needs_connection
+            render :json => {
+              :success => false,
+              :status => "needs_connection",
+              :html => (render_cell :external_accounts, :needs_connection,
+                        :provider => params[:where])
+            }.to_json
+          when :unknown_destination
+            render :json => {
+              # TODO: I18n
+              :success => false,
+              :message => "unknown destination"
+            }.to_json
+          end
+        end
+      end
+    end
   end
 
 end
