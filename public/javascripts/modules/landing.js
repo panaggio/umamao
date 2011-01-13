15d5ae7adb6419dde3abd5fa8743b20cb4611e62
Pedro <fedorius@gmail.com>
2011-01-13 20:31:34 -0300

    Disable button and show spinning wheel after submitting academic email

--- public/javascripts/modules/landing.js
+++ public/javascripts/modules/landing.js
@@ -1,8 +1,15 @@
 function signUpAjaxRequest() {
     var form = $(this).parents("form");
-
+    $("#affiliation_submit").fadeOut("slow", function(){$("#ajax-loader").removeClass('hidden');});
+    $("input#affiliation_email").attr("readonly", "readonly");
+    
     return {
 	  url: "/affiliations?format=js",
-      data: form.serialize()
+      data: form.serialize(),
+      complete: function(){
+		                    $("#ajax-loader").addClass('hidden');
+		                    $("#affiliation_submit").fadeIn("slow");
+		                    $("input#affiliation_email").removeAttr("readonly") ;
+		                  }
     };
  }
