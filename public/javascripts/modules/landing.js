266f6068897e844d3c0e7f49283170ea8f3ba5c4
Pedro <fedorius@gmail.com>
2011-01-14 09:20:31 -0300

    Disabling the button instead of fading it out

--- public/javascripts/modules/landing.js
+++ public/javascripts/modules/landing.js
@@ -1,6 +1,7 @@
 function signUpAjaxRequest() {
     var form = $(this).parents("form");
-    $("#affiliation_submit").fadeOut("slow", function(){$("#ajax-loader").removeClass('hidden');});
+    $("#affiliation_submit").attr("readonly", "readonly");
+    $("#ajax-loader").removeClass('hidden');
     $("input#affiliation_email").attr("readonly", "readonly");
     
     return {
@@ -8,8 +9,8 @@ function signUpAjaxRequest() {
       data: form.serialize(),
       complete: function(){
 		                    $("#ajax-loader").addClass('hidden');
-		                    $("#affiliation_submit").fadeIn("slow");
-		                    $("input#affiliation_email").removeAttr("readonly") ;
+		                    $("#affiliation_submit").removeAttr("readonly");
+		                    $("input#affiliation_email").removeAttr("readonly");
 		                  }
     };
  }
