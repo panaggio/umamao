6bc13ea659ae298955962662e158ce09cc329f0f
Arthur Azevedo de Amorim <arthur@umamao.com>
2011-01-17 16:19:39 -0200

    Removing unused files and code.

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,7 +45,6 @@ class User
   key :following_count,           Integer, :default => 0
 
   key :membership_list,           MembershipList
-  key :university,				  String
 
   key :feed_token,                String
   key :can_invite_without_confirmation, Boolean, :default => true
