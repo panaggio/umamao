6ac45620549dd4ae9213042b36166dc9b39bc4f1
Helder Ribeiro <helder@umamao.com>
2011-01-17 12:15:01 -0200

    Don't show signup form if affiliation is already confirmed.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,6 +60,10 @@ class UsersController < ApplicationController
       @affiliation = Affiliation.
         find_by_affiliation_token(params[:affiliation_token])
 
+      if @affiliation.confirmed_at.present?
+        redirect_to(root_url) && return
+      end
+
       if @affiliation
         @user.affiliation_token = @affiliation.affiliation_token
       end
