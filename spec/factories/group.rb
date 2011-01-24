fae079b7d36521db619da988edee7472d1443d8e
Arthur Azevedo de Amorim <arthur@umamao.com>
2011-01-24 11:08:05 -0200

    Updating factories to reflect current models.

--- spec/factories/group.rb
+++ spec/factories/group.rb
@@ -5,7 +5,6 @@ FactoryGirl.define do
     domain AppConfig.domain
     description "question-and-answer website"
     legend "question and answer website"
-    default_tags %w[tag1 tag2]
     state "active"
   end
 end
