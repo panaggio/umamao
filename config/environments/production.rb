81cb8d15ec84028f0643872f34f1ae165402c110
Helder Ribeiro <helder@umamao.com>
2011-01-30 18:32:24 -0200

    Use memcached for fragment caching.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Shapado::Application.configure do
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
-  config.cache_store = [:file_store, "#{Rails.root}/tmp/cache"]
+  config.cache_store = :dalli_store
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
