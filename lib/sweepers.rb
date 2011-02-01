63ebcd08e51104d796dfeaf81ee767466dd5327f
Ricardo Panaggio <panaggio.ricardo@gmail.com>
2011-02-01 12:41:17 -0200

    Refactor of news expiration; name changed to sweep_news_item and module changed to Sweepers

--- lib/sweepers.rb
+++ lib/sweepers.rb
@@ -7,4 +7,12 @@ module Sweepers
     expire_fragment("question_on_index_#{question.id}_#{question.updated_at}")
     expire_fragment("mini_question_on_index_#{question.id}_#{question.updated_at}")
   end
+
+  def sweep_news_items(question)
+    User.all.each do |u|
+      if u.news_items.include? question
+        expire_fragment("user_news_items_#{u.id}")
+      end
+    end
+  end
 end
