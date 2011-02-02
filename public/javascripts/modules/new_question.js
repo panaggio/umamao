206fed28e19019abeba182675b819ff4de75fee3
Arthur Azevedo de Amorim <arthur@umamao.com>
2011-02-02 12:34:35 -0200

    Fixed WMD problem.

--- public/javascripts/modules/new_question.js
+++ public/javascripts/modules/new_question.js
@@ -1,5 +1,9 @@
-
 $(document).ready(function() {
+
+  // For some reason, WMD throws an exception if its target is hidden.
+  // Thus, we'll only initialize it the first time we display the form.
+  var wmdInit = false;
+
   $("label#rqlabel").hide();
 
   $(".text_field#question_title").focus( function() {
@@ -10,7 +14,12 @@ $(document).ready(function() {
   $("#ask_question #toggle-details").click(function () {
     var details = $("#question-details");
     if (details.is(":hidden")) {
-      details.slideDown("slow");
+      details.slideDown("slow", function () {
+        if (!wmdInit) {
+          $("#question-input").wmdMath({preview: "question-preview"});
+          wmdInit = true;
+        }
+      });
     } else {
       details.slideUp("slow");
     }
