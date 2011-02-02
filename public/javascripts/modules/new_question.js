b5eebde97cfd5a690795c0cbacd0c135ffe968cd
Arthur Azevedo de Amorim <arthur@umamao.com>
2011-02-02 11:44:34 -0200

    Hiding question details by default in questions#new.

--- public/javascripts/modules/new_question.js
+++ public/javascripts/modules/new_question.js
@@ -6,4 +6,18 @@ $(document).ready(function() {
     highlightEffect($("#sidebar .help"));
   });
 
+  // Display/hide question details when asking a new question.
+  $("#ask_question #toggle-details").click(function () {
+    var details = $("#question-details");
+    if (details.is(":hidden")) {
+      details.slideDown("slow");
+    } else {
+      details.slideUp("slow");
+    }
+    var text = $(this).text();
+    var undo = $(this).attr("data-undo");
+    $(this).attr("data-undo", text);
+    $(this).text(undo);
+  });
+
 });
