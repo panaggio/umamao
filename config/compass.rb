c308714e7033f08ca9fc6f1420ff9a11d07a0bf1
Ricardo Panaggio <panaggio.ricardo@gmail.com>
2011-02-03 13:54:02 -0200

    Disable sass commments

--- config/compass.rb
+++ config/compass.rb
@@ -5,6 +5,7 @@ require 'fancy-buttons'
 project_type = :rails
 project_path = Compass::AppIntegration::Rails.root
 environment = Compass::AppIntegration::Rails.env
+line_comments = false
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "public/stylesheets/compiled"
