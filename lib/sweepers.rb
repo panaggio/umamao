beb2898dec2002f07d4804ac6390b947e5a8e319
Ricardo Panaggio <panaggio.ricardo@gmail.com>
2011-02-04 14:46:41 -0200

    Remove old sweepers; caching is disabled, so they aren't needed anymore

--- lib/sweepers.rb
+++ lib/sweepers.rb
@@ -7,12 +7,4 @@ module Sweepers
     expire_fragment("question_on_index_#{question.id}_#{question.updated_at}")
     expire_fragment("mini_question_on_index_#{question.id}_#{question.updated_at}")
   end
-
-  def sweep_news_items(question)
-    User.query.each do |u|
-      if u.news_items.include? question
-        expire_fragment("user_news_items_#{u.id}")
-      end
-    end
-  end
 end
