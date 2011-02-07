0eaecace017602a6f149dfd12d687615cc2a8978
Helder Ribeiro <helder@umamao.com>
2011-02-07 21:25:10 -0200

    This doesn't work.

--- config/application.rb
+++ config/application.rb
@@ -53,9 +53,5 @@ module Shapado
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-
-    config.after_initialize do 
-      Devise::Mailer.handle_asynchronously :confirmation_instructions
-    end
   end
 end
