845321d18b58e8ee18e8da8a3dbe3dd128639912
Arthur Azevedo de Amorim <arthur@umamao.com>
2011-02-11 11:28:11 -0200

    Added our privacy policy and ToS.

--- app/controllers/doc_controller.rb
+++ app/controllers/doc_controller.rb
@@ -1,8 +1,10 @@
 class DocController < ApplicationController
   def privacy
     set_page_title(t("doc.privacy.title"))
+    render :layout => "welcome"
   end
   def tos
     set_page_title(t("doc.tos.title"))
+    render :layout => "welcome"
   end
 end
