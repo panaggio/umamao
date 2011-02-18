7594b930172ba72e454acab4b98e0b3d6825b12e
Alex Grilo <alex@umajaca.(none)>
2011-02-17 22:14:58 -0200

    Add program classes

---
+++ app/models/program_class.rb
@@ -0,0 +1,18 @@
+require 'digest/sha1'
+
+class ProgramClass < Topic
+  include MongoMapper::Document
+
+  key :program_id, String
+  belongs_to :program
+
+  key :year, Integer
+
+  key :student_ids, Array
+  has_many :students, :class_name => 'Student', :in => :student_ids
+
+  validates_presence_of :program_id
+  validates_presence_of :year
+
+  validates_uniqueness_of :year, :scope => :program_id
+end
