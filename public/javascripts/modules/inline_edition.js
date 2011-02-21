dd10d855b45023f58186d2ef00db5a9cbf5a2dd6
Pedro <fedorius@gmail.com>
2011-02-21 10:39:59 -0300

    Added some critical untracked files (I thought they had been added by -a)

---
+++ public/javascripts/modules/inline_edition.js
@@ -0,0 +1,302 @@
+window.inlineEdition = {
+    
+  /* this variable is used to provide communication between 'blur' and
+   * 'keyup' events. */
+  inline_edition_control : false,
+  
+  makeInlineEditable: function (prepare)
+  {
+    var settings = prepare.call(this);
+    $(settings.selector).after("<img class=\"ajax_waiting_image\" src=\"/images/ajax-loader.gif\" />");
+
+    /* When we want the inline edition to be controlled by "edit", "save"
+    * and "cancel" links */
+      if(settings.method == "links") {
+        // Basic DOM changings
+        $(settings.selector)
+	  .addClass("inline_editable")
+	  .after("<a class=\"edit_link inline_link\" href=\"javascript:return false\">Editar </a>")
+	  .after("<a class=\"cancel_link inline_link\" href=\"javascript:return false\">Cancelar </a>")
+	  .after("<a class=\"save_link inline_link\" href=\"javascript:return false\">Salvar </a>");
+
+	$(settings.selector)
+	  .parent()
+	  .addClass("inline_editable_container");
+
+	if(typeof(settings.adjustments) == "function") {
+          settings.adjustments();
+	}
+
+        // Edit link
+	$(".edit_link").live("click", function(event) {
+	  $(event.target).siblings('a.inline_link')
+            .addClass("active_inline_link")
+	    .removeClass("inline_link");
+			     
+	  // Making the input
+	  var inp = inlineEdition.replaceByInput($(event.target)
+	    .siblings('.inline_editable'));
+
+	  if(typeof(settings.placeSaveAndCancelLinks) != "function" ||
+             settings.placeSaveAndCancelLinks($(event.target), inp) == false) {
+
+	    // Positioning "save" and "cancel" buttons.
+	    var spacing = 16; //Spacing between input and inline_links
+
+	    var i_left = inp.offset().left;
+	    var i_width = inp.width();
+	    var s_width = $(event.target).siblings('a.save_link').width();
+
+	    $(event.target).siblings('a.save_link')
+	      .css("left", i_left + i_width + spacing)
+              .css("top", inp.offset().top);
+
+	    $(event.target).siblings('a.cancel_link')
+	      .css("left", i_left + i_width + s_width + 2.5*spacing)
+              .css("top", inp.offset().top);
+
+	  }
+	  // Hides the "edit"
+	  $(event.target).removeClass('edit_link').addClass('inactive_edit_link');
+
+      });
+
+      // Cancel link
+      $('.cancel_link').live('click',
+        function(event) {
+	  inlineEdition.cancel($(':input', $(event.target).parent()), settings);
+          $(event.target).siblings('.active_inline_link')
+	    .add(event.target)
+	    .removeClass("active_inline_link")
+	    .addClass('inline_link');
+
+          $(event.target).siblings('.inactive_edit_link')
+            .removeClass('inactive_edit_link')
+            .addClass('edit_link');
+      });
+
+      // Save link		   
+      $('.save_link').live('click',
+        function(event) {
+          var sentRequest = inlineEdition.save($(':input', $(event.target).parent()), settings,
+	    function(elem) { //error
+	      elem.siblings('.inactive_edit_link')
+	        .removeClass('inactive_edit_link')
+                .addClass('edit_link');
+
+              $('.active_ajax_waiting_image', elem.parent())
+                .removeClass('.active_ajax_waiting_image')
+                .addClass('ajax_waiting_image');  
+	    },
+
+	    function(elem) { //success
+	      elem.parent().siblings('.inactive_edit_link')
+                .removeClass('inactive_edit_link')
+                .addClass('edit_link');
+
+              elem.parent().siblings('.active_ajax_waiting_image')
+	        .removeClass('.active_ajax_waiting_image')
+                .addClass('ajax_waiting_image');
+	    });
+
+	  if(sentRequest)
+            $('.ajax_waiting_image', $(event.target).parent())
+              .removeClass('ajax_waiting_image')
+              .addClass('active_ajax_waiting_image');
+	  else
+	    $(event.target).siblings('.inactive_edit_link')
+	      .removeClass('inactive_edit_link')
+              .addClass('edit_link');
+
+          $(event.target).siblings('.active_inline_link')
+	    .add(event.target)
+	    .removeClass("active_inline_link")
+	    .addClass('inline_link'); 
+        }
+      );
+    }
+	    
+    /* When we want the inline edition to be controlled by "double click"
+     * "enter" and "blur" */
+    if(settings.method == "dblclick")
+    {
+      $(settings.selector).live("dblclick", function(event) {
+        inlineEdition.replaceByInput($(event.target));
+      }).addClass('inline_dblclick_editable');
+		
+      // Second, set the blur event (Will this be here?)
+      $(settings.selector).live("blur", function(event) {
+        if(inlineEdition.inline_edition_control)
+          inlineEdition.inline_edition_control = false;
+        else
+	  $(event.target).parent().text($(event.target).attr("data-previous-value"));
+        });
+		
+      // Third, set the keyup for save on enter
+      $(settings.selector).live("keyup", function(event) {
+	if(event.keyCode == "13") { //keyCode 13 is an enter
+	  // Control to prevent onblur event to overwrite this results
+	  inlineEdition.inline_edition_control = true;
+			 
+          // Sets locally          
+	  inlineEdition.save($(event.target), settings);
+
+          // Show ajax waiting image
+          $('.ajax_waiting_image', $(event.target).parent().parent()).removeClass('ajax_waiting_image').addClass('active_ajax_waiting_image');
+        }
+      });
+    }
+  },
+
+  cancel: function(elem, settings) {
+
+    if(elem.parent().attr('data-inline-edition-markdown') &&
+      !elem.parent().hasClass('empty_inline_editable_field'))
+    {
+      var converter = new Showdown.converter();
+      elem.parent().html(converter.makeHtml(elem.attr("data-previous-value")));
+    }
+    else
+      elem.parent().html(elem.attr("data-previous-value"));
+  },
+
+  save: function(elem, settings, error, success) {
+    if(elem.val() == elem.attr('data-previous-value') || 
+        (elem.parent().hasClass('empty_inline_editable_field') &&
+	 elem.val()=='')) {
+      inlineEdition.cancel(elem, settings);
+      return false;
+    }
+
+    var parent = elem;
+    while(!parent.attr("data-inline-object-key"))
+      parent = parent.parent();
+
+    elem.attr('readonly','readonly');
+
+    var ajaxParams = {
+    url: settings.url,
+      data: {
+        name: elem.parent().attr("data-inline-name"),
+        value: elem.val(),
+        inline_object_key: parent.attr("data-inline-object-key")
+      },
+
+      type: "POST",
+
+      error: function(data) {
+	Utils.showMessage('sorry, something went wrong.', "error");
+
+	if(typeof(error) == "function")
+	  error.call(null, elem);
+
+	elem.parent().html(elem.attr('data-previous-value'));
+      },
+
+      success: function(data) {
+	elem.removeAttr('readonly');
+
+        if(data.error.length > 0)
+	  Utils.showMessage(data.error, "error");
+
+	if(data.empty_field)
+	  elem.parent().addClass('empty_inline_editable_field');
+	else
+	  elem.parent().removeClass('empty_inline_editable_field');
+
+        if(elem.parent().attr("data-inline-edition-editable-content"))
+	  elem.parent().attr("data-inline-edition-editable-content", data.value);
+
+	if(typeof(success) == "function")
+	  success.call(null, elem);
+
+        if(elem.parent().attr('data-inline-edition-markdown') &&
+          !elem.parent().hasClass('empty_inline_editable_field')) {
+
+	  elem.parent().attr('data-inline-edition-editable-content', data.value);
+
+	  var converter = new Showdown.converter();
+          elem.parent().html(converter.makeHtml(data.value));
+	}
+	else
+	  elem.parent().html(data.value);
+      }
+    }
+    $.ajax(ajaxParams);
+    return true;
+  },
+	
+  replaceByInput: function(elem) {
+    var text = elem.attr("data-inline-edition-editable-content") || elem.text();
+
+    var inputType = elem.attr('data-inline-edition-input');
+    if(inputType == "textarea")
+    {
+      if(elem.hasClass('empty_inline_editable_field'))
+        elem.html('<textarea resizable="resizable"></textarea>');
+      else
+        elem.html('<textarea resizable="resizable" style="height'+elem.css('height')+'">'+text+'</textarea>');
+    }
+    else
+    {
+      if(elem.hasClass('empty_inline_editable_field'))
+        elem.html('<input type="text"/>');
+      else
+        elem.html('<input value="'+text+'"type="text"/>');
+    }
+
+    return $(":input ", elem).attr("data-previous-value", text).focus();
+  },
+
+  resetView: function() {    
+    $('.active_inline_link').removeClass('active_inline_link');
+    $('.active_ajax_waiting_image').removeClass('.active_ajax_waiting_image').addClass('ajax_waiting_image');
+  }
+}
+
+function universityInlineEdition() {
+  return {
+    //Can be either 'links' or 'dblclick'
+    method: "dblclick",
+	    
+    //The url that will be called to handle the edition
+    url: "/universities/inline_edition"
+  }
+}
+
+function profileInlineEdition() {
+  return {
+    // Can be either 'links' or 'dblclick'
+    method: "links",
+	    
+    // The url that will be called to handle the edition
+    url: "/users/inline_edition",
+
+    //
+    selector: ".profile_inline_editable[data-inline-name], .profile_inline_editable *[data-inline-name]",
+
+    placeSaveAndCancelLinks: function(elem, inp) {
+      /* Place the "save" and "cancel" link in the bottom instead of after.
+       * (only if is edition description)
+       * Takes the edit link as the input as parameter. */
+
+       if(elem.siblings("*[data-inline-name]").attr("data-inline-name") != "description")
+	 return false;
+
+       else if(elem.siblings("*[data-inline-name]").attr("data-inline-name") != "description")
+	 return false;
+
+       var spacing = 24; //Spacing between save and cancel links
+
+       var e_left = elem.offset().left;
+       var s_width = elem.siblings('a.save_link').width();
+
+       elem.siblings('a.save_link')
+         .css("left", e_left);
+
+       elem.siblings('a.cancel_link')
+	 .css("left", e_left + s_width + spacing);
+
+    }
+  }
+}
