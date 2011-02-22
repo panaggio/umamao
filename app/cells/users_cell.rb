1b0088f5a9a6fac177018b49d7c96ccdd91b7e03
Arthur Azevedo de Amorim <arthur@umamao.com>
2011-02-22 09:56:56 -0300

    First experiments with followe{rs,d} caching in user profile.

--- app/cells/users_cell.rb
+++ app/cells/users_cell.rb
@@ -3,10 +3,15 @@ class UsersCell < Cell::Rails
   helper ApplicationHelper
   helper UsersHelper
 
+  cache :list do |cell, options|
+    cell.options[:cache_key]
+  end
+
   # List of users with small avatars.
   def list
-    @users = @opts[:users]
-    @title = @opts[:title]
+    @users = options[:users]
+    @title = options[:title]
+    @cache_key = options[:cache_key]
     render
   end
 
