8263717b2320f7c365541e5002bffe7fb5197bc0
Arthur Azevedo de Amorim <arthur@umamao.com>
2011-02-22 13:27:00 -0300

    Using cell caching for followe{r,d} list.

--- app/cells/users_cell.rb
+++ app/cells/users_cell.rb
@@ -3,15 +3,32 @@ class UsersCell < Cell::Rails
   helper ApplicationHelper
   helper UsersHelper
 
-  cache :list do |cell, options|
-    cell.options[:cache_key]
+  cache :followers do |cell, options|
+    user = cell.options[:user]
+    "#{user.id}/#{user.followers.count}"
+  end
+
+  cache :following do |cell, options|
+    user = cell.options[:user]
+    "#{user.id}/#{user.following.count}"
+  end
+
+  def followers
+    @user = options[:user]
+    @users = @user.followers
+    render
+  end
+
+  def following
+    @user = options[:user]
+    @users = @user.following
+    render
   end
 
   # List of users with small avatars.
   def list
-    @users = options[:users]
-    @title = options[:title]
-    @cache_key = options[:cache_key]
+    @users ||= options[:users]
+    @title ||= options[:title]
     render
   end
 
