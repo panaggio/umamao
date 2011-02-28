eb8451b83c640d274edae4b82688d26cd978fc84
Ricardo Panaggio <panaggio.ricardo@gmail.com>
2011-02-28 16:19:04 -0300

    Full rewrite of wikipedia sax parser

--- lib/wikipedia_parser.rb
+++ lib/wikipedia_parser.rb
@@ -1,39 +1,55 @@
 require "nokogiri"
 
 class WikipediaPagesArticleDumpParser < Nokogiri::XML::SAX::Document
-  def  start_document
-    @inside = {
-      "page" => false,
-      "title" => false,
-      "id" => false
-    }
-
-    @topics = []
+  def start_document
+    set_internals false
   end
 
   def end_document
-    File.open("tmp/wikipedia_hash_dump", "w") do |f|
-      f.write(YAML.dump @topics)
-    end
+    set_internals nil, nil
   end
 
   def start_element(element, attributes = [])
-    @inside[element] = true
-    @topic = {} if element == "page"
+    @inside = element
+    case element
+    when "page"
+      @topic = {}
+      @inside_page = true
+    when "revision"
+      @inside_revision = true
+    end
   end
 
   def end_element(element)
-    @inside[element] = false
-    @topics << @topic if element == "page"
+    @inside = nil
+    case element
+    when "page"
+      unless @topic.nil?
+        title  = @topic.delete "title"
+        yield @topic
+        @topic = nil
+      end
+      @inside_page = false
+    when "revision"
+      @inside_revision = false
+    end
   end
   
   def characters(text)
-    state = @inside.select{|s,status| status}[0]
-    if ["title", "id"].include? state
-      @topic[state] = text
+    if ["title", "id"].include? @inside and @inside_page and not @inside_revision
+      @topic[@inside] = (@topic[@inside] || "") << text
     end
+
+    if @inside == "text" and @inside_revision
+      @topic = nil if text.include? "#REDIRECT"
+    end
+  end
+
+  protected
+  def set_internals(status)
+    @inside_page, @inside_revision = status, status
   end
 end
 
-parser = Nokogiri::XML::SAX::Parser.new(WikipediaPagesArticleDumpParser.new)
-parser.parse(File.open(ARGV[0]))
+# parser = Nokogiri::XML::SAX::Parser.new(WikipediaPagesArticleDumpParser.new)
+# parser.parse(File.open(filename)
