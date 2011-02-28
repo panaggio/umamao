cfb821654c08de17862401aef95520425cf410b9
Arthur Azevedo de Amorim <arthur@umamao.com>
2011-02-28 15:44:54 -0300

    Restructured Javascript assets.

---
+++ public/assets/content_edit.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("textarea").focus(function(){if(!window.onbeforeunload){window.onbeforeunload=function(){var a=false;$("textarea").each(function(){if($.trim($(this).val())!=""){a=true}});if(a){return I18n.on_leave_page}return null}}})});
