bd871fd5b3e341aad7934fdd7591db23bb1595bf
Ricardo Panaggio <panaggio.ricardo@gmail.com>
2011-03-01 13:34:22 -0300

    Heavy rewrite of wikipedia parser
    * Add DJ to fillin the blanks on the wikipedia and freebase keys
      of Topic
    * refactor the way to detect redirects
    * refactor names of variables

--- lib/wikipedia_parser.rb
+++ lib/wikipedia_parser.rb
@@ -14,10 +14,12 @@ class WikipediaPagesArticleDumpParser < Nokogiri::XML::SAX::Document
     @inside = element
     case element
     when "page"
-      @topic = {}
+      @article = {}
       @inside_page = true
     when "revision"
       @inside_revision = true
+    when "redirect"
+      @redirects = true
     end
   end
 
@@ -25,10 +27,11 @@ class WikipediaPagesArticleDumpParser < Nokogiri::XML::SAX::Document
     @inside = nil
     case element
     when "page"
-      unless @topic.nil?
-        send_outside @topic
-        @topic = nil
+      if not @redirects and @article
+        send_outside @article
+        @article = nil
       end
+      @redirects = false
       @inside_page = false
     when "revision"
       @inside_revision = false
@@ -37,55 +40,69 @@ class WikipediaPagesArticleDumpParser < Nokogiri::XML::SAX::Document
   
   def characters(text)
     if ["title", "id"].include? @inside and @inside_page and not @inside_revision
-      @topic[@inside] = (@topic[@inside] || "") << text
-    end
-
-    if @inside == "text" and @inside_revision
-      @topic = nil if text.include? "#REDIRECT"
+      @article[@inside] = (@article[@inside] || "") << text
     end
   end
 
   protected
   def set_internals(status)
-    @inside_page, @inside_revision = status, status
+    @inside_page, @inside_revision, @redirects = status, status, status
   end
 
   def send_outside article
-    WikipediaTopicCreator.enqueue_topic article
+    WikipediaTopicCreator.enqueue_article article
   end
 
   def finish_processing
-    WikipediaTopicCreator.pull_topics
+    WikipediaTopicCreator.pull_articles
   end
 end
 
 module WikipediaTopicCreator
-  WINDOW_SIZE = 100
+  WINDOW_SIZE = 10_000
 
-  def self.enqueue_topic(article)
-    @topics ||= {}
+  def self.enqueue_article(article)
     title = article.delete("title")
-    @topics["title"] = article
-    create_topics if @topics.size > WINDOW_SIZE
+    @articles ||= {}
+    @articles[title] = article
+    self.create_topics if @articles.size > WINDOW_SIZE
   end
 
-  def self.pull_topics
-    create_topics unless @topics.empty?
+  def self.pull_articles
+    self.create_topics unless @articles.empty?
   end
 
   def self.create_topics
-    Topic.from_titles!(@topics).each do |topic|
-      topic.wikipedia_pt_id = article["id"]
+    titles = @articles.keys.map{ |t| t.dup }
 
-      #q = Freebase::MidQuery[article["id"]].results[0]
-      #topic.freebase_mids = q.mids
-      #topic.wikipedia_pt_key = q.pt_article.slug
-      #topic.description = q.pt_article.description
+    Topic.from_titles!(titles).each do |topic|
+      begin
+        article = @articles.delete(topic.title)
+        topic.wikipedia_pt_id = article["id"]
+        topic.save
+      rescue
+        topic.wikipedia_import_status =
+          if article.nil?
+            "parse error: empty article"
+          else
+            "unkown error"
+          end
+        topic.save
+      end
 
-      topic.save
+      self.delay.fillin_topics(topic, article)
     end
-    p "created #{@topics.size} topics @ #{Time.now}"
 
-    @topics = {}
+    @articles = {}
+  end
+
+  def self.fillin_topics(topic, article)
+    q = Freebase::MidQuery[article["id"].to_i].results[0]
+
+    topic.freebase_mids = q.mids
+    topic.wikipedia_pt_key = q.pt_article.slug
+    topic.description = q.pt_article.description
+
+    topic.save
   end
 end
