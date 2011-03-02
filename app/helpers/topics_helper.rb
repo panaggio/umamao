3100f0cfb71ce8a1dd96f0d6e8d6a120c4bd0ae9
Arthur Azevedo de Amorim <arthur@umamao.com>
2011-03-02 11:14:31 -0300

    Migration to convert tags in versions into topics.

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,5 +1,6 @@
 module TopicsHelper
 
+  # Small topic container used throughout the site.
   def topic_box(topic)
     '<li><div class="topic"><span class="topic-title">' +
       (link_to h(topic.title), topic_url(topic)) +
