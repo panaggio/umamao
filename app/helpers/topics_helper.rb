225eb4e46dfacb3013f4a3b64c4f590924f56b2d
Alex Bredariol Grilo <alex@umamao.com>
2011-03-09 08:48:32 -0300

    Use application helper truncate method

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -2,7 +2,7 @@ module TopicsHelper
 
   def topic_help_text(topic)
     if topic.description.present?
-      cut_description(remove_links(topic.description))
+      truncate_words(remove_links(topic.description), 100)
     else
       topic.title
     end
@@ -14,11 +14,4 @@ module TopicsHelper
     description.gsub(/\[([^\]]*)\]\[\d*\]/, '\1').gsub(/\[\d*\]: [^ ]*/, '').gsub(/ +/, " ").gsub(/[\r\n]/, '')
   end
 
-  def cut_description(description)
-    if description.length > 100
-      "#{description[0, 97]}..."
-    else
-      description
-    end
-  end
 end
