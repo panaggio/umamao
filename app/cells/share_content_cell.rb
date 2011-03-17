842697543f94f494efa42768ac673dfde46238fd
Ricardo Panaggio <panaggio.ricardo@gmail.com>
2011-03-17 14:29:34 -0300

    Create share_content from share_question
    share_content is a generic implementation of share_question and can
    be adapted to work with content of other classes.

---
+++ app/cells/share_content_cell.rb
@@ -0,0 +1,21 @@
+class ShareContentCell < Cell::Rails
+  helper ApplicationHelper
+
+  # Displays a dialog box to share content
+  # (questions, answers, ...) on other site.
+  def display
+    @body = options[:body]
+    @content = options[:content]
+    @class_name = options[:class_name]
+    @where = options[:where]
+    @link = options[:link]
+    @maxlength = case @where
+    when"twitter"
+      140
+    when "facebook"
+      420
+    end
+    render
+  end
+
+end
---
+++ app/controllers/share_content_controller.rb
@@ -0,0 +1,163 @@
+# This controller is used to share content on other websites, such as
+# posting a question or an answer on Facebook or Twitter.
+
+# whenever this class is inherited, a constant named CONTENT_CLASS
+# must be defined on the child class and set to the class that will be
+# shared
+# For instance, if the content is a Question, CONTENT_CLASS should be:
+#
+# class ShareQuestionController < ShareContentController
+#   CONTENT_CLASS = Question
+# end
+class ShareContentController < ApplicationController
+  before_filter :login_required
+  before_filter :check_connections
+
+  def new
+    @content = content_class.find_by_id(params[:content])
+
+    respond_to do |format|
+      format.js do
+        html = {
+          :content => @content,
+          :class_name => content_class_str,
+          :body => default_body,
+          :where => params[:where]
+        }
+        if params[:where] == "twitter"
+          bitly = Bitly.new(AppConfig.bitly[:username], AppConfig.bitly[:apikey])
+          html[:link] = bitly.shorten(content_url(@content)).short_url
+        end
+        render :json => {
+          :success => true,
+          :html => (render_cell :share_content, :display, html)
+        }
+      end
+    end
+  end
+
+  def create
+    @body = params[:body]
+    @content = content_class.find_by_id(params[:content])
+    @link = content_url(@content)
+    status = :success
+
+    case params[:where]
+    when "facebook"
+      begin
+        graph = current_user.facebook_connection
+        # FIXME: how can we get an image url inside a controller?
+        graph.put_wall_post(@body, :link => @link,
+                            :picture => AppConfig.site + "/images/logosquare.png")
+        status = :success
+      rescue Koala::Facebook::APIError
+        status = :needs_permission
+        session["omniauth_return_url"] = content_path(@content)
+      end
+    when "twitter"
+      begin
+        client = current_user.twitter_client
+        client.update(@body)
+        status = :success
+      end
+    end
+
+    if status == :success
+      message = I18n.t("#{content_class_str.pluralize}.show.share_success", :site => params[:where].capitalize)
+      track_event("shared_#{content_class_str}".to_sym, :where => params[:where])
+    end
+
+    respond_to do |format|
+      format.html do
+        redirect_to content_path(@content)
+      end
+
+      format.js do
+        case status
+        when :success
+          render :json => {
+            :success => true,
+            :message => message
+          }.to_json
+        when :needs_permission
+          render :json => {
+            :success => false,
+            :status => "needs_permission",
+            :html => (render_cell :external_accounts, :needs_permission,
+                      :provider => params[:where])
+          }.to_json
+        end
+      end
+    end
+  end
+
+  # Check whether the corresponding external account is actually present,
+  # asking the user to connect it if it isn't.
+  def check_connections
+    status = :success
+
+    case params[:where]
+    when "facebook"
+      if !current_user.facebook_account
+        status = :needs_connection
+      end
+    when "twitter"
+      status = :needs_connection if !current_user.twitter_client
+    else
+      status = :unknown_destination
+    end
+
+    if status != :success
+      respond_to do |format|
+        format.js do
+          case status
+          when :needs_connection
+            session["omniauth_return_url"] =
+              content_url(content_class.find_by_id(params[:content]))
+            render :json => {
+              :success => false,
+              :status => "needs_connection",
+              :html => (render_cell :external_accounts, :needs_connection,
+                        :provider => params[:where])
+            }.to_json
+          when :unknown_destination
+            render :json => {
+              :success => false,
+              :message => I18n.t("#{content_class_str.pluralize}.show.unknown_destination")
+            }.to_json
+          end
+        end
+      end
+    end
+  end
+
+  protected
+  def content_class
+    self.class::CONTENT_CLASS
+  end
+
+  def content_class_str
+    @@content_class_str ||= content_class.to_s.underscore
+  end
+
+  def content_class_sym
+    @@content_class_sym ||= content_class_str.to_sym
+  end
+
+  def content_url(content)
+    self.send("#{content_class_str}_url".to_sym, content)
+  end
+
+  def content_path(content)
+    self.send("#{content_class_str}_path".to_sym, content)
+  end
+
+  # Facebook: "Is this a good question?"
+  # Twitter:  "Umamão: Is this a good question? http://bit.ly/umamao"
+  def default_body
+    {
+      'facebook' => @content.title,
+      'twitter' => "#{AppConfig.application_name}: #{@content.title}"
+    }
+  end
+end
