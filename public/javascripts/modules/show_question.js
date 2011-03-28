518d1000629d618c9fa83933567f6597d011341a
Ricardo Panaggio <panaggio.ricardo@gmail.com>
2011-03-28 14:20:36 -0300

    Add negative counter to share modal

--- public/javascripts/modules/show_question.js
+++ public/javascripts/modules/show_question.js
@@ -90,14 +90,29 @@ $(document).ready(function() {
   $("#body").live("keyup", function() {
     var t = $(this);
     var len = t.val().length;
-    var maxlen = t.attr('maxlength');
+    var maxlen = t.attr('data-maxlength');
+    var charcount = $('.charcount');
+    var charcount_text = $('.charcount span:last');
     var charsleft = $('.charsleft');
-    if (len < maxlen)
-      charsleft.html(maxlen - len);
-    else {
-      t.val(t.val().substr(0,maxlen));
-      charsleft.html(0);
+    cl = maxlen - len;
+
+    if (cl >= 0) {
+      if (cl > 1 || cl == 0)
+        charcount_text.html("caracteres restantes");
+      else
+        charcount_text.html("caracter restante");
+      charcount.css("color", "black");
     }
+    else {
+      if (cl == -1)
+        charcount_text.html("caracter excedente");
+      else
+        charcount_text.html("caracteres excedentes");
+      charcount.css("color", "red");
+      cl = -cl;
+    }
+
+    charsleft.html(cl);
   });
 
   // Send shared question to Facebook.
