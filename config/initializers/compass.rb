2ffd59fde1272288477701230759637dd23b21e4
Arthur Azevedo de Amorim <arthur@umamao.com>
2011-04-06 13:12:37 -0300

    Disable compass in production.

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,3 +1,5 @@
-require 'compass'
-require 'compass/app_integration/rails'
-Compass::AppIntegration::Rails.initialize!
+if AppConfig.enable_compass
+  require 'compass'
+  require 'compass/app_integration/rails'
+  Compass::AppIntegration::Rails.initialize!
+end
