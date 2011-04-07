8bf3e2f3e7dde7f406f0d7618d4c8e97793f7ecf
Ricardo Panaggio <panaggio.ricardo@gmail.com>
2011-04-07 09:22:35 -0300

    Add ignored topics subtab to settings

--- app/controllers/settings/ignore_topics_controller.rb
+++ app/controllers/settings/ignore_topics_controller.rb
@@ -1,14 +1,16 @@
 # Bulk-following of topics in user settings.
 
-class Settings::FollowTopicsController < ApplicationController
+class Settings::IgnoreTopicsController < ApplicationController
   before_filter :login_required
   layout 'settings'
   set_tab :follow_topics, :settings
 
   def edit
-    @active = 'follow'
-    @topics = Topic.query(:follower_ids => current_user.id).
+    @active = 'ignore'
+    @topics = current_user.ignored_topics.
       paginate(:per_page => 100, :page => params[:page] || 1)
+
+    render 'settings/follow_topics/edit'
   end
 
 end
