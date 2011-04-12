07b5ff6a723a8094fbee21aa3a9e741165012f24
Ricardo Panaggio <panaggio.ricardo@gmail.com>
2011-04-12 17:03:52 -0300

    Validates user_suggestion
    user_suggestions can only be created if user don't follow entry and if
    that suggestion has not already been made

--- app/models/user_suggestion.rb
+++ app/models/user_suggestion.rb
@@ -2,4 +2,15 @@ class UserSuggestion < Suggestion
   include MongoMapper::Document
   key :origin_id, :required => true, :index => true
   belongs_to :origin, :class_name => 'User'
+
+  validates_uniqueness_of :user_id, :scope => [ :origin_id, :entry_id ]
+
+  validate :user_dont_follow_entry?
+
+  def user_dont_follow_entry?
+    if self.user.following?(self.entry)
+      self.errors.add(:user_id,
+                      "User already follows that entry")
+    end
+  end
 end
