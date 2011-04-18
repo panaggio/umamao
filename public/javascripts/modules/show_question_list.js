5541f5bfa872ca579b4851a080669f1b80fe5982
Arthur Azevedo de Amorim <arthur@umamao.com>
2011-04-18 11:21:24 -0300

    Open question list upload to everyone. UI.

--- public/javascripts/modules/show_question_list.js
+++ public/javascripts/modules/show_question_list.js
@@ -1,15 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  var wmdInit = false;
+
   initTopicAutocompleteForReclassifying();
 
-  $("#question_body").wmdMath({preview: "question-preview"});
-
-  $("#question-list #new_question_form").hide();
-
-  Utils.clickObject("#question-list #question_submit", function() {
+  // Question submit
+  Utils.clickObject("#question-list #question_submit", function () {
     return {
-      success: function(data) {
+      success: function (data) {
         var question = $(data.html);
         $("#question-list #questions").append(question);
+        question.animate({ backgroundColor: "#ffffcb" },
+                         { duration: 100,
+                           complete: function () {
+                             question.animate({ backgroundColor: "white" }, 2000);
+                           }
+                         }
+        );
         MathJax.Hub.Queue(['Typeset', MathJax.Hub, question[0]]);
         $("#new_question_form .editor-input").val("");
         $("#questions .empty").remove();
@@ -22,26 +28,33 @@ $(document).ready(function() {
     };
   });
 
-  $("#question-list #new_question_link").click(function() {
-    $("#question-list #new_question_form").show();
-    $("#question-list #new_question_link").hide();
+  // Add new questions
+  $("#question-list #add_content .add_questions").click(function () {
     var prefix = $("#question-list h1.navtitle").text().trim();
     var questionTitleInput = $("#question-title-input");
-    if (questionTitleInput.length != 0) {
-      questionTitleInput.val(prefix + " - ");
-      questionTitleInput.focus();
-      window.location.hash = "form";
+    $("#question-list #add_content").hide();
+    $("#question-list #new_question_form").show();
+    questionTitleInput.val(prefix + " - ");
+    questionTitleInput.focus();
+    if (!wmdInit) {
+      $("#question_body").wmdMath({preview: "question-preview"});
+      wmdInit = true;
     }
   });
 
-  console.log(window.location.hash);
-  if (window.location.hash == "#form" || window.location.hash == "form") {
-    $("#question-list #new_question_link").click();
-  }
-
-  $("#new_question_form .cancel").click(function() {
+  $("#new_question_form .cancel").click(function () {
+    $("#question-list #add_content").show();
     $("#question-list #new_question_form").hide();
-    $("#question-list #new_question_link").show();
-    window.location.hash = "";
+  });
+
+  // Add new files
+  $("#question-list #add_content .add_files").click(function () {
+    $("#question-list #add_content").hide();
+    $("#question-list #add_file_form").show();
+  });
+
+  $("#add_file_form .cancel").click(function () {
+    $("#question-list #add_content").show();
+    $("#question-list #add_file_form").hide();
   });
 });
