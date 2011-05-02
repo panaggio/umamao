bd96fa2ca27cfc18b012e705aef3b0b14383fc2e
Ricardo Panaggio <panaggio.ricardo@gmail.com>
2011-05-02 16:27:36 -0300

    Refactor unanswered questions counter

--- app/controllers/question_lists_controller.rb
+++ app/controllers/question_lists_controller.rb
@@ -184,8 +184,7 @@ class QuestionListsController < TopicsController
 
   protected
   def calculate_unanswered_count
-    QuestionList.find_by_slug_or_id(params[:id]).questions.count(
-      :is_open => true)
+    @question_list.questions.count(:is_open => true)
   end
 
   def main_topic_allow_question_lists
