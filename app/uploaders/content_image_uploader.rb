b8d875d68342d0cff334aa852249d80f2305085c
Arthur Azevedo de Amorim <arthur@umamao.com>
2011-05-06 16:26:31 -0300

    Fix resize scheme for content images.

--- app/uploaders/content_image_uploader.rb
+++ app/uploaders/content_image_uploader.rb
@@ -3,7 +3,7 @@ class ContentImageUploader < FileUploader
   include CarrierWave::RMagick
 
   version :large do
-    process :resize_to_fit => [500, 300]
+    process :resize_to_limit => [500, 300]
   end
 
   def store_dir
