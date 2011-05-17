3dc12cfd8d99732a8361427a0de8d3c1f2e25adc
Arthur Azevedo de Amorim <arthur@umamao.com>
2011-05-17 09:00:58 -0300

    Extend tests.

--- test/unit/question_list_test.rb
+++ test/unit/question_list_test.rb
@@ -11,16 +11,23 @@ class QuestionListTest < ActiveSupport::TestCase
     Delayed::Job.delete_all
 
     @main_topic = Factory.create(:topic)
-    @topic1 = Factory.create(:topic)
-    @topic2 = Factory.create(:topic)
+    @topic = Factory.create(:topic)
   end
 
   test "should classify questions after topic is included" do
     question_list = Factory.create(:question_list, :main_topic => @main_topic)
     question = Factory.create(:question)
     question.classify!(question_list)
-    question_list.classify!(@topic1)
-    assert(question.topics.include?(@topic1))
+    question_list.classify!(@topic)
+    assert(question.reload.topics.include?(@topic))
+  end
+
+  test "should unclassify questions after topic is removed" do
+    question_list = Factory.create(:question_list, :main_topic => @main_topic)
+    question = Factory.create(:question, :topics => [@topic, question_list])
+    question_list.classify!(@topic)
+    question_list.unclassify!(@topic)
+    assert(!question.reload.topics.include?(@topic))
   end
 
 end
