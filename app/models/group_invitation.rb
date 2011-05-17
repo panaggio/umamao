8e896a0644aa59ecc708a21edf37fbc5062f7572
Alex Bredariol Grilo <alex@umamao.com>
2011-05-17 09:21:58 -0300

    Revert wrong change in comment

--- app/models/group_invitation.rb
+++ app/models/group_invitation.rb
@@ -1,9 +1,10 @@
-# Group invitations are l URLs that allow users to sign up
-# without an academic email (eg. example.com/slashdot). It also
+# Group invitations are special URLs that allow users to sign up
+# without an academic email (e.g. example.com/slashdot). It also
 # allows displaying a special message and tracking users who sign up
 # through that URL, so it's good for marketing campaigns.
 class GroupInvitation
   include MongoMapper::Document
+
   key :slug, String, :unique => true
   key :message, String
   key :active, Boolean, :default => true
